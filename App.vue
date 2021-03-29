8364f6edadbb6f7a34626003640de85f06ec65f4
aniket-0103 <75750757+aniket-0103@users.noreply.github.com>
2021-03-29 18:05:40 +0530

    The root component of the webpage
    Components are parts/elements of a webpage referred by a named instance while making program with vue.js .

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+   <eve />
+  </div>
+</template>
+
+<script>
+import eve from './components/events.vue'
+
+export default {
+  name: 'App',
+  components: {
+    eve
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
